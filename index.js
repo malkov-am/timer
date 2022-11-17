278926e8fd5615dde65061fddc3b371bf926ed32
Andrei Malkov <thewildcat@yandex.ru>
2022-11-17 22:07:35 +0300

    Add timer

--- index.js
+++ index.js
@@ -1,24 +1,62 @@
-const inputEl = document.querySelector('input');
-const buttonEl = document.querySelector('button');
-const timerEl = document.querySelector('span');
+const inputEl = document.querySelector("input");
+const buttonEl = document.querySelector("button");
+const timerEl = document.querySelector("span");
 
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  // Функция, переводящая секунды в формат ЧЧ:ММ:СС
+  const formatTime = (seconds) => {
+    const hoursLeft = Math.floor(seconds / 3600);
+    const minutesLeft = Math.floor((seconds - hoursLeft * 3600) / 60);
+    const secondsLeft = seconds - hoursLeft * 3600 - minutesLeft * 60;
+    // Функция, добавляющая впереди 0, если число имеет всего 1 разряд
+    const addZeroSymbol = (number) => {
+      return number.toString().padStart(2, "0");
+    };
+    return `${addZeroSymbol(hoursLeft)}:${addZeroSymbol(
+      minutesLeft
+    )}:${addZeroSymbol(secondsLeft)}`;
+  };
+
+  let timerInterval = null;
+
+  const stopTimer = () => {
+    clearInterval(timerInterval);
+    alert("Время вышло!");
+  };
+
+  const startTimer = (seconds) => {
+    let timePassed = 0;
+    let timeLeft = seconds;
+    timerInterval = setInterval(() => {
+      // Увеличиваем счетчик прошедшего времени на 1 секунду
+      timePassed += 1;
+      // Вычисляем оставшееся время
+      timeLeft = seconds - timePassed;
+      // Отрисовываем остаток времени на странице
+      timerEl.textContent = formatTime(timeLeft);
+      // Останавливаем таймер по истечению времени
+      timeLeft === 0 && stopTimer();
+    }, 1000);
+  };
+
+  return (seconds) => {
+    startTimer(seconds);
+  };
 };
 
 const animateTimer = createTimerAnimator();
 
-inputEl.addEventListener('input', () => {
+inputEl.addEventListener("input", () => {
   // Очистите input так, чтобы в значении
   // оставались только числа
 });
 
-buttonEl.addEventListener('click', () => {
+buttonEl.addEventListener("click", () => {
   const seconds = Number(inputEl.value);
 
   animateTimer(seconds);
 
-  inputEl.value = '';
+  inputEl.value = "";
 });
